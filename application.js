625ad8cc47a5849c1027fcd07b17d5d40b62eec8
Dale Campbell <dale@save-state.net>
2009-07-01 20:31:30 -0500

    wowz

---
+++ application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.show-info').click(function() {
+    var info = $(this).siblings('.info');
+    $('.info').slideUp('fast'); // Hide the other info divs.
+    info.slideDown('fast');
+    return false;
+  });
+}); // $(document).ready()
