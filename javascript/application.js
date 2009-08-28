91557d658f9e0ac313615f1387f8140a7a229a6c
Dale Campbell <oshuma@gmail.com>
2009-08-27 20:49:38 -0500

    layout stuff

--- javascript/application.js
+++ javascript/application.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
 
-  $('.show-info').click(function() {
+  $('#tabs a').click(function() {
+    $.get(this.href, function(page) {
+      $('#content').html(page);
+    });
+    return false;
+  });
+
+  $('#projects .show-info').live('click', function() {
     var info = $(this).siblings('.info');
     $('.info').slideUp('fast'); // Hide the other info divs.
     info.slideDown('fast');
